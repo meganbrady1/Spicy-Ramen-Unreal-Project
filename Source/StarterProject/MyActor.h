f2ef2f5ac334859a88cd5ca9d6675cd36fb74e4f
meganbrady1 <46699185+meganbrady1@users.noreply.github.com>
2020-12-03 20:28:23 -0700

    UI changes + countdown timer

---
+++ Source/StarterProject/MyActor.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class STARTERPROJECT_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+		float TestFloat;
+		
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+
+};
